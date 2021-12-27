5dbc3464e698425a1465e373576c9e3395f0f398
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-12-27 13:20:22 +0000

    #1 Update notes to be compatible with the project structure

--- taniguchi_2021_analysis/models.py
+++ taniguchi_2021_analysis/models.py
@@ -98,7 +98,7 @@ def estimate_baseline(T_cal, order=1, weight=1.0):
     model.fit(X, y, sample_weight=weight)
 
     # estimate baseline
-    T_base = xr.full_like(T_cal, model.coef_ @ X.T)
+    T_base = xr.zeros_like(T_cal) + model.coef_ @ X.T
 
     for i in range(n_poly):
         T_base.coords[f"basis_{i}"] = "ch", X[:, i]
